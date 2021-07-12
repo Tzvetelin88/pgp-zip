fe4fc207dc133361b990fa1cf1ecd2328022ea9e
Tsvetelin Bleskov <tbleskov@vmware.com>
2021-07-12 11:34:31 +0300

    v2 working with binary files

--- pgp.ts
+++ pgp.ts
@@ -17,7 +17,7 @@ import fs from 'fs'
     });
 
     // For POC I don't use Keys for the moment, only password
-    await encryptFileAndCreateReplacement('test.zip', 'asdasdasdsada');
+    // await encryptFileAndCreateReplacement('test.zip', 'asdasdasdsada');
     await decryptFileAndCreateReplacement('test-enc.zip', 'asdasdasdsada');
 
 })();
@@ -25,112 +25,36 @@ import fs from 'fs'
 openpgp.config.allowUnauthenticatedStream = true;
 
 
-async function encryptFileSym(fileBuffer: fs.ReadStream, secret: string) {
-  const message = await openpgp.createMessage({ text: fileBuffer, type: 'binary' });
+async function encryptFileSym(fileBuffer: Buffer, secret: string) {
+  const message = await openpgp.createMessage({ binary: fileBuffer });
   return await openpgp.encrypt({
       message: message,
       passwords: [secret],
-      armor: false, 
+      armor: false,
   });
 }
 
-async function decryptFileSym(fileBuffer: fs.ReadStream, secret: string) {
-  const message = await openpgp.readMessage({ armoredMessage: fileBuffer });
+async function decryptFileSym(fileBuffer: Buffer, secret: string) {
+  const message = await openpgp.readMessage({ binaryMessage: fileBuffer });
   return await openpgp.decrypt({
       message: message,
       passwords: [secret], // decrypt with password
       format: 'binary' // output as Uint8Array
   });
-
 }
 
 async function encryptFileAndCreateReplacement(filename: string, secret: string) {
   const newFileName = filename.split('.')[0] + "-enc.zip";
-  const oldFileBuff = fs.createReadStream(filename);
+
+  const oldFileBuff = fs.readFileSync(filename);
   const encryptedData = await encryptFileSym(oldFileBuff, secret);
-  const newFileStream = fs.createWriteStream(newFileName);
-  encryptedData.pipe(newFileStream);
+  fs.appendFileSync(newFileName, Buffer.from(encryptedData));
 }
 
 async function decryptFileAndCreateReplacement(filename: string, secret: string) {
   const newFileName = filename.replace("enc", "dec");
-  //delete the old file
-  //delete the encrypted secret file
-  const oldFileBuff = fs.createReadStream(filename);
-  const decryptedData = await decryptFileSym(oldFileBuff, secret);
-  const newFileStream = fs.createWriteStream(newFileName);
-  decryptedData.data.pipe(newFileStream);
+
+  const oldFileBuff = fs.readFileSync(filename);
+  const encryptedData = await decryptFileSym(oldFileBuff, secret);
+  fs.writeFileSync(newFileName, Buffer.from(encryptedData.data))
 }
-
-
-// Not used data, examples....
-
-// (async function () {
-//   await encryptFileAndCreateReplacement("test.txt", "helloworld");
-//   await decryptFileAndCreateReplacement("test.enc", "helloworld");
-// })();
-
-
-
-// async function encryptFileSym(fileBuffer: fs.ReadStream, secret: string) {
-//   const oldFileBuff = fs.createReadStream('test.txt');
-//   const message = await openpgp.createMessage({ text: <fs.ReadStream>fileBuffer });
-//   return await openpgp.encrypt({
-//       message,
-//       passwords: secret,
-//       armor: false, 
-//   });
-// }
-// async function decryptFileSym(fileBuffer: fs.ReadStream, secret: string) {
-//   const message = await openpgp.readMessage({ armoredMessage: fileBuffer });
-//   return await openpgp.decrypt({
-//       message,
-//       passwords: secret,
-//   });
-// }
-
-
-
-
-// const publicKeyArmored = fs.readFileSync('./public.key', {
-//   encoding: 'utf8',
-//   flag: 'r'
-// });
-// (async () => {
-//   const plainData = fs.readFileSync('secret.txt');
-//   const encrypted = await openpgp.encrypt({
-//     message: openpgp.message.fromText(plainData),
-//     publicKeys: (await openpgp.key.readArmored(publicKeyArmored)).keys
-//   });
-//   fs.writeFileSync('encrypted-secret.txt', encrypted.data);
-//   console.log(`data has been encrypted...`);
-// })();
-
-
-// async function encrypt1() {
-//   const data = await fs.createReadStream("test.txt")
-//   const encrypted = await openpgp.encrypt({
-//     message: openpgp.createMessage.(
-//       new Uint8Array([0x01, 0x01, 0x01])
-//     ),
-//     publicKeys: (await openpgp.key.readArmored(publicKeyArmored)).keys,
-//   });
-//   let readStream = encrypted.data;
-//   let writeStream = fs.createWriteStream("encrypted-test.txt", {
-//     flags: "a",
-//   });
-//   readStream.pipe(writeStream);
-//   readStream.on("end", () => console.log("done!"));
-// }
-
-// async function encrypt() {
-//   const plainData = fs.readFileSync("test.txt");
-//   const encrypted = await openpgp.encrypt({
-//     message: openpgp.message.fromText(plainData),
-//     publicKeys: (await openpgp.key.readArmored(publicKeyArmored)).keys,
-//   });
-
-//   fs.writeFileSync("encrypted-test.txt", encrypted.data);
-// }
-
-// encrypt();
